052313fd9f1682b74e7c7399456aa047961650d9
Antti <33394028+antti3@users.noreply.github.com>
2022-05-02 17:16:45 +0300

    Update streamlit_app.py
    edited snowflake section to look nicer and fetch all rows instead of just one

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display normalized response
 streamlit.dataframe(fruityvice_normalized)
 
-# Snowflake connector debug data
+# Fetch data from Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+my_data_row = my_cur.fetchall()
+
+# Display data from Snowflake
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
