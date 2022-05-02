88907cbe13051f278c9870026d19204242933c3b
Antti <33394028+antti3@users.noreply.github.com>
2022-05-02 17:40:38 +0300

    Update streamlit_app.py
    moved debug marker

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,6 @@ try:
     streamlit.dataframe(response_from_function)
 except URLError as e:
   streamlit.error()
-    
-# DEBUG: don't run anything after this
-streamlit.stop()
 
 # Display data from Snowflake
 streamlit.header("The fruit load list contains:")
@@ -57,6 +54,9 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+  
+# DEBUG: don't run anything after this
+streamlit.stop()
 
 # Allow the user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
